b2b65e7dc7466b39d51e67d876bd34fe0b81e776
Cem <cemsertkaya99@gmail.com>
2022-06-23 15:28:10 +0300

    UI: Navigation Bar is completed.

---
+++ RickAndMortyApp/UI+.swift
@@ -0,0 +1,22 @@
+//
+//  UI+.swift
+//  RickAndMortyApp
+//
+//  Created by Cem Sertkaya on 23.06.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor
+{
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor
+    {
+        
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+}
--- RickAndMortyApp/ViewController.swift
+++ RickAndMortyApp/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    private let navigationTitle : UILabel =
+    {
+        let titleLabel = UILabel()
+        titleLabel.text = "Rick and Morty"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        titleLabel.textColor = UIColor.black
+        titleLabel.sizeToFit()
+        return titleLabel
+    }()
+    
+    private let navigationButton : UIButton =
+    {
+        let navBarButton = UIButton(type: .custom)
+        if let image = UIImage(named: "filterAsset"){navBarButton.setImage(image, for: .normal)}
+        return navBarButton
+    }()
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.init().UIColorFromHex(rgbValue: 0xE5E5E5, alpha: 1)
+        
+        self.view.addSubview(navigationTitle)
+        navigationTitle.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(120)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.view.addSubview(navigationButton)
+        navigationButton.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(115)
+            make.right.equalToSuperview().offset(-20)
+        }
+        
     }
 
+    
+   
 
 }
+
 
